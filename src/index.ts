f38505e7126eb46d96ae1a097248666087383a61
Abdulrehman Ahmad <abdulrehman821337@gmail.com>
2023-05-23 08:36:34 +1000

    learnt types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-let age: number = 20;
-if (age < 50) {
-    age += 10;
+/* types */
+let sales: number = 123_456_789;
+let course: string = "TypeScript";
+let is_published: boolean = true;
+let level; /* any type but loses features of TS */
+
+function render(document: any) {
+  console.log(document);
 }
-console.log(age);
+
+/* arrays */
+let numbers = [1, 2, 3];
