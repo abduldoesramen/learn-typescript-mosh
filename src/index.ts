fc85ff902913c8eb771c9d5d23bd8b14c659c6ec
Abdulrehman Ahmad <abdulrehman821337@gmail.com>
2023-05-23 08:53:19 +1000

    functions basics

--- src/index.ts
+++ src/index.ts
@@ -31,3 +31,11 @@ const enum Size {
 
 let mySize: Size = Size.Medium;
 console.log(mySize);
+
+/* Functions */
+function calculateTax(income: number, taxYear: number): number {
+  if (taxYear < 2022) return income * 1.2;
+  return income * 1.3;
+}
+
+calculateTax(10_000, 2022);
